a2d10408cc8ab9d5788f8ca7edbf0191d4247e93
Jamie <eejl773@aucklanduni.ac.nz>
2023-06-15 00:35:02 +1200

    synced time across timezones

--- src/components/DisplayWeather.jsx
+++ src/components/DisplayWeather.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function DisplayWeather({ weatherData, timeData }) {
+  const [apiTime, setApiTime] = useState(null);
+  const [localTime, setLocalTime] = useState(new Date());
+
+  useEffect(() => {
+    if (!timeData) {
+      return;
+    }
+
+    const apiDateTime = new Date(timeData.location.localtime);
+    setApiTime(apiDateTime);
+  }, [timeData]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setLocalTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const getFormattedTime = (date) => {
+    const hours = apiTime ? apiTime.getHours() : timeData.location.localtime.split(' ')[1].slice(0, 5);
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
   if (!weatherData || !timeData) {
     return null;
   }
 
+
   return (
-    <div className="mt-5 p-4 rounded-mdr text-center font-serif font-family-Times border border-gray-100 rounded-2xl">
+    <div className="mt-5 p-4 w-64 rounded-mdr text-center font-serif font-family-Times border border-gray-100 rounded-2xl">
       <h2 className="mb-2 text-2xl font-semibold italic">
         {weatherData.location.name}
       </h2>
       <p className="mb-2">
-        Local time: {timeData.location.localtime}
+        Local time: {getFormattedTime(localTime)}
       </p>
       <img
           src={weatherData.current.condition.icon}
