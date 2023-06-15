6ebe63d58e33a25b61930fc261d1f0b27ca57612
Jamie <eejl773@aucklanduni.ac.nz>
2023-06-15 15:42:03 +1200

    modified files config files in prep to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/WeatherAppV1/',
+  base: '/WeatherAppV1',
   plugins: [react()],
 })
